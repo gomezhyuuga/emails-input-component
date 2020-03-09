1a282ced81263fe6c00553981de9b52d2e68e56d
Fernando Gomez <fernando.gomez2@scouting.org>
2020-03-09 01:32:48 +0100

    LIB: export global defaults

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,22 @@ import EmailBlock from './EmailBlock';
 
 // var inputContainerNode = document.querySelector('#emails-input');
 // var emailsInput = EmailsInput(inputContainerNode, { ...options });
-console.log('adding emails...');
-const emails: EmailBlock[] = [];
-emails.push(new EmailBlock('john@miro.com'));
-emails.push(new EmailBlock('invalid.email'));
-emails.push(new EmailBlock('mike@miro.com'));
-emails.push(new EmailBlock('alexander@miro.com'));
-console.log('EMAILS', emails);
-const sandbox = document.querySelector('#sandbox');
-if (sandbox) emails.forEach(email => sandbox.appendChild(email.wrapper));
+// console.log('adding emails...');
+// const emails: EmailBlock[] = [];
+// emails.push(new EmailBlock('john@miro.com'));
+// emails.push(new EmailBlock('invalid.email'));
+// emails.push(new EmailBlock('mike@miro.com'));
+// emails.push(new EmailBlock('alexander@miro.com'));
+// console.log('EMAILS', emails);
+// const sandbox = document.querySelector('#sandbox');
+// if (sandbox) emails.forEach(email => sandbox.appendChild(email.wrapper));
 // Handling Add email and Get emails count buttons, etc.
 
 function exportGlobals() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).EmailsInput = EmailsInput;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).EmailBlock = EmailBlock;
 }
 
 if (window) exportGlobals();
