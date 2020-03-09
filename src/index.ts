21d446553cd7fdbf04c2a137d485094309d5ea33
Fernando Gomez <fernando.gomez2@scouting.org>
2020-03-09 16:08:29 +0100

    INDEX: improve demo with random email

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
 import EmailsInput from './EmailsInput';
 import EmailBlock from './EmailBlock';
-
-// var inputContainerNode = document.querySelector('#emails-input');
-// var emailsInput = EmailsInput(inputContainerNode, { ...options });
-// console.log('adding emails...');
-// const emails: EmailBlock[] = [];
-// emails.push(new EmailBlock('john@miro.com'));
-// emails.push(new EmailBlock('invalid.email'));
-// emails.push(new EmailBlock('mike@miro.com'));
-// emails.push(new EmailBlock('alexander@miro.com'));
-// console.log('EMAILS', emails);
-// const sandbox = document.querySelector('#sandbox');
-// if (sandbox) emails.forEach(email => sandbox.appendChild(email.wrapper));
-// Handling Add email and Get emails count buttons, etc.
+import { generateRandomEmail } from './utils';
 
 function exportGlobals() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).EmailsInput = EmailsInput;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).EmailBlock = EmailBlock;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).__randomEmail = generateRandomEmail;
 }
 
 if (window) exportGlobals();
